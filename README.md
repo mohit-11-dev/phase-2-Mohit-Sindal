6626b911f23e23f5a0307ff543e4d5d6aca4477b
Your Name <you@example.com>
2021-04-18 20:16:54 +0530

    Project files added

---
+++ README.md
@@ -0,0 +1,1 @@
+# phase-2-Mohit-Sindal
---
+++ src/com/client/Client.java
@@ -0,0 +1,24 @@
+package com.client;
+
+import com.dao.HibernateDaoImpl;
+import com.dto.ClassInfo;
+import com.dto.Student;
+
+public class Client {
+
+	public Client() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		HibernateDaoImpl hibernateDaoImpl=new HibernateDaoImpl();
+//		 ClassInfo classInfo=new ClassInfo();
+//	        classInfo.setName("Class 2");
+//	        Student student = new Student();
+//	        student.setName("Shyam");
+//	        classInfo.setStudent(student);
+//	        hibernateDaoImpl.addClassInfo(classInfo);
+	}
+
+}
